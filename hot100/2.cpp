9574a08cb29943840f7fee876913cbc328cef2f1
MurmanskY <642015814@qq.com>
2025-07-14 16:33:23 +0800

    hot100_2 solu update

--- hot100/2.cpp
+++ hot100/2.cpp
@@ -29,6 +29,7 @@ strs[i] 仅包含小写字母
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <map>
 using namespace std;
 
 class Solution {
@@ -59,14 +60,43 @@ class Solution {
 };
 
 
+class Solution2 {
+    //  使用unordered_map收集映射后的字符串
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<vector<int>, vector<string>> mp;
+        for (auto str : strs) {
+            vector<int> str_hash(26, 0);
+            vector<string> str_set;
+            for (auto ch : str) {
+                str_hash[ch - 'a'] += 1;
+            }
 
+            auto it = mp.find(str_hash);
+            if (it != mp.end()) {
+                // 非空
+                str_set = it->second;
+            }
+
+            str_set.push_back(str);
+            mp[str_hash] = str_set;
+        }
+
+        for (auto it = mp.begin(); it != mp.end(); it++) {
+            ans.push_back(it->second);
+        }
+
+        return ans;
+    }
+};
 
 
 
 
 int main() {
     vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    Solution solu;
+    Solution2 solu;
     vector<vector<string>> ans = solu.groupAnagrams(strs);
     for (auto group : ans) {
         for (auto str : group) {
