27b5423e9f5bb6ee2a5900d3c0a291e3dcf3e7a2
MurmanskY <642015814@qq.com>
2025-07-14 15:04:50 +0800

    hot100_1 update hash solu

--- hot100/1.cpp
+++ hot100/1.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
@@ -21,11 +22,29 @@ class Solution {
     }
 };
 
+
+class Solution2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for ( int i = 0; i < nums.size(); i++ ) {
+            auto it = mp.find(target - nums[i]);
+            if ( it != mp.end() ) { // 已经找到了
+                ans.push_back(it->second);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]] = i;
+        }
+    }
+};
+
 int main() {
     vector<int> nums = {2, 7, 11, 15};
-    Solution solu1;
-    solu1.twoSum(nums, 9);
-    for (int& i : nums) {
+    Solution2 solu;
+    vector<int> ans = solu.twoSum(nums, 9);
+    for (auto i : ans) {
         cout << i << endl;
     }
     return 0;
