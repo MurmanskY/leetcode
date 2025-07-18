b1f5453b864e98a5f1902c638c0eae02a488eca5
MurmanskY <642015814@qq.com>
2025-07-18 13:27:11 +0800

    hot100_10 double que solution

--- hot100/11.cpp
+++ hot100/11.cpp
@@ -3,6 +3,7 @@
 //
 # include <iostream>
 # include <vector>
+# include <deque>
 # include <algorithm>
 
 using namespace std;
@@ -49,8 +50,45 @@ public:
 
 
 
+class Solution2 {
+    // 使用 双端队列的方法，在滑动窗口中实时维护最大值
+    // 双端队列中，从头到尾，value递减
+    // 有点点难理解
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q;
+        // 初始化
+        for (int i = 0; i < k; i++) {
+            while (!q.empty() && nums[i] >= nums[q.back()]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+        }
+
+        ans.push_back(nums[q.front()]); // 初试化
+
+        for (int i = k; i < nums.size(); i ++) {
+            while (!q.empty() && nums[i] >= nums[q.back()]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+
+            while(q.front() <= i - k) {
+                q.pop_front();
+            }
+            ans.push_back(nums[q.front()]);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
 int main() {
-    Solution solu;
+    Solution2 solu;
     vector<int> nums = {1,3,1,2,0,5};
     vector<int> result = solu.maxSlidingWindow(nums, 3);
 
