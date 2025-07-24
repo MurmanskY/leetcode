66eb9fbdc9f56f74a3f3053711ddead2a226972f
MurmanskY <642015814@qq.com>
2025-07-24 14:28:31 +0800

    hot 100 22 update solution

--- hot100/22.cpp
+++ hot100/22.cpp
@@ -2,46 +2,56 @@
 // Created by mumansk on 25-7-23.
 //
 # include <iostream>
-# include <vector>
 
 
 using namespace std;
 
 
+
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+
 };
+
+
+
+
+void printList(ListNode *head) {
+    ListNode *p = head;
+    while (p != nullptr) {
+        cout << p->val << " ";
+        p = p->next;
+    }
+    return;
+}
+
+
+
 
 class Solution {
 public:
-    // 在两条链表上交替遍历。
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == nullptr || headB == nullptr) {
-            return nullptr; // 如果存在空链表，返回空
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {return nullptr;}
+
+        ListNode * tail = head->next;
+        while(tail != nullptr) {
+            ListNode * temp = tail->next;
+            head->next = temp;
+            ListNode * new_head = new ListNode(tail->val, head);
+            head = new_head;
+            tail = temp; // tail往后移一个
         }
-        ListNode * pA = headA, *pB = headB;
-        while (pA != pB) {
-            if (pA == nullptr) {
-                pA = headB;
-            } else {
-                pA = pA->next;
-            }
-
-            if (pB == nullptr) {
-                pB = headA;
-            } else {
-                pB = pB->next;
-            }
-
-        }
-
-        return pA;
+        return head;
     }
 };
 
 
 int main() {
+    ListNode *head = new ListNode(1);
+    printList(head);
     return 0;
 }
